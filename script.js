0d008b27e1136c3878906261559f209b09a80a99
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-11 14:38:31 +0200

    Update flowchart and add game board creation

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentPlayer = 'x';
+const NUMBER_OF_ROWS = 3;
+const turns = NUMBER_OF_ROWS ** 2;
+let turnsCounter = 0; 
+
+const createBoard = ()=>{
+	const container = document.querySelector(".container");
+	const board = document.createElement("div");
+
+	board.classList.add('board');
+
+	for (let i = 0; i < turns; i++) {
+		const cellElementString = `<div class='cell'><span class='value'></span></div>`;
+		const cellElement = document.createRange().createContextualFragment(cellElementString) ;
+		
+		cellElement.querySelector(".cell").onclick = (event) =>console.log("youclickedme");
+		board.appendChild(cellElement);
+	}
+container.insertAdjacentElement('afterbegin',board);
+}
+
+createBoard();
