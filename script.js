ccf7e8071fa72609884097f3d7ff5d9dfead2ad6
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-11 15:12:46 +0200

    getting position of a cell

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const NUMBER_OF_ROWS = 3;
 const turns = NUMBER_OF_ROWS ** 2;
 let turnsCounter = 0; 
 
+const getCellPlacement = (index,numberOfRows)=>{
+	const row = Math.floor(index / numberOfRows);
+	const col = index % numberOfRows;
+
+	return [row,col]
+}
+
+const cellClickHandler =(event,index) =>{
+const cell = event.target;
+
+const placement = getCellPlacement(index,NUMBER_OF_ROWS);
+
+console.log({placement});
+ }
+
 const createBoard = ()=>{
 	const container = document.querySelector(".container");
 	const board = document.createElement("div");
@@ -13,7 +28,7 @@ const createBoard = ()=>{
 		const cellElementString = `<div class='cell'><span class='value'></span></div>`;
 		const cellElement = document.createRange().createContextualFragment(cellElementString) ;
 		
-		cellElement.querySelector(".cell").onclick = (event) =>console.log("youclickedme");
+		cellElement.querySelector(".cell").onclick = (event) =>cellClickHandler(event,i);
 		board.appendChild(cellElement);
 		document.documentElement.style.setProperty("--grid-rows",NUMBER_OF_ROWS);
 	}
